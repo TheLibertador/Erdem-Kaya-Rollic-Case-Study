69aa8c77b6a5a97665bded1cc09b5430c66d133e
Erdem Kaya <195040055@stu.istinye.edu.tr>
2022-11-21 11:23:04 +0300

    Edit: FinalPit ScriptableObject and removed unnecessery line in Game Manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -11,7 +11,6 @@ public class GameManager : MonoBehaviour
 
     [NonSerialized] public bool gameStopped = true;
     [NonSerialized] public bool gameFinished = false;
-    [NonSerialized] public bool retryLevel = false;
     [NonSerialized] public float pitWaitTime = 1.5f;
 
     
